158fe8b54184be5c848a4f63e06e62b0c7b65e9a
Han Lee <lee46@mail.usf.edu>
2024-10-30 07:43:59 -0400

    Initial Commit

---
+++ hangpt/hangptApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangptApp.swift
+//  hangpt
+//
+//  Created by Han Lee on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct hangptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hangptTests/hangptTests.swift
@@ -0,0 +1,17 @@
+//
+//  hangptTests.swift
+//  hangptTests
+//
+//  Created by Han Lee on 10/30/24.
+//
+
+import Testing
+@testable import hangpt
+
+struct hangptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
